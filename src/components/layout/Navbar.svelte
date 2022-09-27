197e32a1a6792f2f4b5531fa5e17a503455b1821
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-27 11:57:58 -0400

    first commit

---
+++ src/components/layout/Navbar.svelte
@@ -0,0 +1,15 @@
+
+  <div class="container mx-auto">
+    <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a href="/" class="btn btn-ghost normal-case text-xl">
+            <img width="40px" alt="logo"src="/assets/img/logo.svg"></a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal p-0">
+            <li><a href="/">Login</a></li>
+            <li><a href="/">Register</a></li>
+          </ul>
+        </div>
+      </div>
+  </div>
